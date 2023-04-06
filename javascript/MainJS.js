8e49a6f1a07fc50867f26013aee94a932e556556
Mitsutan <petitcom_hondou@icloud.com>
2023-04-06 12:43:39 +0900

    animation

--- javascript/MainJS.js
+++ javascript/MainJS.js
@@ -3,16 +3,6 @@ function jumpTo(pageID) {
     if (target) scrollBy(0, target.getBoundingClientRect().top);
 }
 
-//const elAbout = document.getElementById('h1About');
-//const animFireCkAbout = new IntersectionObserver(fireAnimetionAbout, { threshold: 0.5 });
-//animFireCkSkills.observe(elSkills);
-
-//function fireAnimetionSkills(el) {
-//    if (el[0].isIntersecting) {
-//        elSkills.classList.add('anime1');
-//    }
-//}
-
 const elWindowCrossAnimeFire = document.querySelectorAll('.windowCrossAnimeFire');
 const animFireCk = new IntersectionObserver(fireAnimation, { threshold: 1 });
 
@@ -22,8 +12,14 @@ elWindowCrossAnimeFire.forEach(function (val) {
 
 function fireAnimation(el) {
     el.forEach(function (val) {
+        //console.log(val.target.nodeName);
         if (val.isIntersecting) {
-            val.target.classList.add('anime1');
+            if (val.target.nodeName == 'H1') {
+                val.target.classList.add('anime1');
+            } else {
+                val.target.classList.add('anime3');
+            }
+            
         }
     });
 }
